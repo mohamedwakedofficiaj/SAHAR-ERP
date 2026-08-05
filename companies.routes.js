b2fbd4b517b058c353dc806fa83ed2deab955625
mohamedwakedofficiaj <mohamedwakedofficial@gmail.com>
2026-08-05 12:24:38 +0300

    Update companies.routes.js

--- companies.routes.js
+++ companies.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./db/pool');
 
-// جلب جميع الشركات
 router.get('/', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM companies');
