bbfa953d3b095bddcd411edbdc99da37683ce14d
mohamedwakedofficiaj <mohamedwakedofficial@gmail.com>
2026-08-05 12:24:10 +0300

    Update pool.js

--- pool.js
+++ pool.js
@@ -1,12 +1,8 @@
 const { Pool } = require('pg');
-require('dotenv').config();
-
-// إلغاء تشفير SSL إذا كان الاتصال داخلياً على Railway
-const isRailway = process.env.DATABASE_URL && process.env.DATABASE_URL.includes('railway.internal');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isRailway ? false : (process.env.DATABASE_URL ? { rejectUnauthorized: false } : false)
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
